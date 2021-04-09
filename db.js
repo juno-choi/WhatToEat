50a0ec72b1a6b1667d00fc429da0d074302b2340
jhchoi <ililil9482@naver.com>
2021-04-09 15:53:04 +0900

    추가 : 위치기반 랜덤뽑기 <설명> javascript의 geolocation을 사용하여 위치의 경도와 위도 값을 획득한 였다. node에서 DB에서의 해당 위치에서의 반경 distance값을 1로 고정한 값으로 구한뒤 rownum을 붙였고, random값을 통해 rownum을 랜덤하게 집어와 해당 내용을 화면에 뿌려준다.

--- db.js
+++ db.js
@@ -40,6 +40,18 @@ function getOneRestaurant(params, callback){
         callback(rows);
     });
 }
+
+function getOneRestaurantApi(params, callback){
+    connection.query(
+        `select * from (
+            SELECT @rownum:=@rownum+1 as rownum, r.* 
+            FROM RESTAURANT_API as r, (select @rownum:=0) tmp) 
+        as x 
+        where rownum = ?`, [params.idx], (err, rows, fields) =>{
+        if(err) throw err;
+        callback(rows);
+    });
+}
 function insertRestaurant(params, callback){
     connection.query(`INSERT INTO RESTAURANT (KIND, SEPERATE, NAME, MENU) VALUES (?,?,?,?)`, [params.kind, params.seperate, params.name, params.menu], (err, rows, fields) =>{
         if(err) throw err;
@@ -53,7 +65,54 @@ function deleteRestaurant(params, callback){
     });
 }
 
+function insertRestaurantApi(json, callback){
+    connection.query(`INSERT INTO RESTAURANT_API (SIGUN_NM, BIZPLC_NM, SANITTN_BIZCOND_NM, REFINE_ROADNM_ADDR, REFINE_LOTNO_ADDR, REFINE_ZIP_CD, REFINE_WGS84_LAT, REFINE_WGS84_LOGT) VALUES (?,?,?,?,?,?,?,?)`, 
+                                                [json.SIGUN_NM, json.BIZPLC_NM, json.SANITTN_BIZCOND_NM, json.REFINE_ROADNM_ADDR, json.REFINE_LOTNO_ADDR, json.REFINE_ZIPNO, json.REFINE_WGS84_LAT, json.REFINE_WGS84_LOGT], (err, rows, fields) =>{
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function getRestaurantApiCount(callback){
+    connection.query(`SELECT COUNT(IDX) AS CNT FROM RESTAURANT_API`, (err, rows, fields) =>{
+        if(err) throw err;
+        callback(rows);
+        return rows[0].CNT;
+    });
+}
+
+function getRestaurantApiCountByPlace(param, callback){
+    connection.query(`
+    select count(idx) as CNT from
+    (
+        SELECT *,
+        (6371*acos(cos(radians(?))*cos(radians(REFINE_WGS84_LAT))*cos(radians(REFINE_WGS84_LOGT)
+        -radians(?))+sin(radians(?))*sin(radians(REFINE_WGS84_LAT))))
+        AS distance
+        FROM RESTAURANT_API
+        HAVING distance <= ?
+    ) as x`,[param.lat, param.logt, param.lat, param.distance], (err, rows, fields) =>{
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
+function getRestaurantApiByPlace(param, callback){
+    connection.query(`
+    select * from (
+        SELECT @rownum:=@rownum+1 as rownum, ra.*,
+            (6371*acos(cos(radians(?))*cos(radians(REFINE_WGS84_LAT))*cos(radians(REFINE_WGS84_LOGT)
+            -radians(?))+sin(radians(?))*sin(radians(REFINE_WGS84_LAT))))
+            AS distance
+        FROM RESTAURANT_API as ra, (select @rownum:=0) tmp
+            HAVING distance <= ?
+        ) as x where rownum = ? ;`,[param.lat, param.logt, param.lat, param.distance, param.random], (err, rows, fields) =>{
+        if(err) throw err;
+        callback(rows);
+    });
+}
+
 module.exports = {
-    getAllTester, getAllRestaurant, getOneRestaurant, insertRestaurant, deleteRestaurant
+    getAllTester, getAllRestaurant, getOneRestaurant, insertRestaurant, deleteRestaurant, insertRestaurantApi, getOneRestaurantApi, getRestaurantApiCount, getRestaurantApiByPlace, getRestaurantApiCountByPlace
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('./../db');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,6 +22,30 @@ router.get('/randomRestaurant' , function(req, res, next){
   db.getOneRestaurant(params, (result)=>{
     res.send({result : result});
   });
+});
+
+router.get('/placeRandomRestaurant' , function(req, res, next){
+
+  console.log(req.query);
+  let params = {};
+  params.lat = req.query.lat;
+  params.logt = req.query.logt;
+  params.distance = 1;
+  let restaurantApiCount;
+  new Promise((resolve, reject)=>{
+    db.getRestaurantApiCountByPlace(params, (result)=>{
+      restaurantApiCount = result;
+      resolve();
+    });
+  }).then(()=>{
+    const random = Math.floor(Math.random()*(restaurantApiCount[0].CNT))+1;
+    params.random = random;
+    db.getRestaurantApiByPlace(params, (result)=>{
+    res.send({result : result});
+  });
+  });
+  
+  
 });
 
 router.get('/postRestaurant' , function(req, res, next){
@@ -44,4 +69,26 @@ router.post('/deleteRestaurant' , function(req, res, next){
     res.send({flag:true, result:result});
   });
 });
+
+/* router.get('/readFile' , function(req, res, next){
+  const jsonFile = fs.readFileSync('public/china.json','utf-8');
+  console.log('파일 파싱');
+
+  const jsonData = JSON.parse(jsonFile);
+  console.log('json 파싱');
+  
+  for(let i=0; i<jsonData.length; i++){
+    if(jsonData[i].BSN_STATE_NM == '영업'){
+      db.insertRestaurantApi(jsonData[i], (result, err)=>{
+        if(err){
+          console.log('실패');
+        }else{
+          console.log('성공 > '+i);
+        }
+      });
+    }
+  }
+  console.log('파일 읽기 종료');
+}); */
+
 module.exports = router;
